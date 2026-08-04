ff6ffa6b0085247c3ee6ab009f57ccaaebf75883
Jerry <jerry@local>
2026-08-04 20:10:18 +0800

    chore: record v3 install version 0.90.4119

--- src/build-info.ts
+++ src/build-info.ts
@@ -2,6 +2,6 @@
 export interface BuildInfo { mode: 'dev' | 'release'; version: string; buildTime: string }
 export const buildInfo: BuildInfo = {
   mode: 'release',
-  version: '0.90.4118',
-  buildTime: '2026-08-04T11:55:54.815Z'
+  version: '0.90.4119',
+  buildTime: '2026-08-04T12:08:57.056Z'
 };
