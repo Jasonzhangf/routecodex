daa10e9949cc37d9053bf133058aa67e08ff3b80
Jerry <jerry@local>
2026-08-09 00:01:47 +0800

    chore: bump version to 0.90.4231 (install:v3 build info)

--- src/build-info.ts
+++ src/build-info.ts
@@ -2,6 +2,6 @@
 export interface BuildInfo { mode: 'dev' | 'release'; version: string; buildTime: string }
 export const buildInfo: BuildInfo = {
   mode: 'release',
-  version: '0.90.4230',
-  buildTime: '2026-08-08T15:39:08.952Z'
+  version: '0.90.4231',
+  buildTime: '2026-08-08T15:57:22.703Z'
 };
